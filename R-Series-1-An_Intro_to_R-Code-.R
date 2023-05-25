2c3967f834d350f2edf29b27b8a15490c65891b8
Tom Perry <tom.perry@dhsc.gov.uk>
2023-05-25 19:36:53 +0100

    Skeleton of the series ready for pasting in the rest of the code once finalised.

---
+++ R-Series-1-An_Intro_to_R-Code-.R
@@ -0,0 +1,34 @@
+###########################################################
+# ---------------------------------------------------------
+#         GFSA Programming Teach-ins - R - Series 1        
+# ---------------------------------------------------------
+###########################################################
+
+######## Some really useful guides for learning R:
+########    R for Data Science - Hadley Wickham & Garrett Grolemund: https://r4ds.had.co.nz/r-markdown.html
+########    R Cheatsheets - Posit: https://posit.co/resources/cheatsheets/
+########    Introduction to R - Analysis Function: https://learninghub.ons.gov.uk/course/view.php?id=498
+
+###########################
+# -------------------------
+#         Session 1        Introduction to R Studio and Data Types in R
+# -------------------------
+###########################
+
+###########################
+# -------------------------
+#         Session 2        Data Manipulation
+# -------------------------
+###########################
+
+###########################
+# -------------------------
+#         Session 3        Data Analysis
+# -------------------------
+###########################
+
+###########################
+# -------------------------
+#         Session 4        Data Visualisation
+# -------------------------
+###########################
